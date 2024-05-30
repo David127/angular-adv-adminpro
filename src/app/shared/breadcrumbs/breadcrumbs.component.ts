d786a8d9f66317f25a405db8545ccb78b2d2b51d
David Huillca <david@MacBook-Air-de-David.local>
2024-05-29 22:17:22 -0500

    Fin sección 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +9,32 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+
+  titulo: string = '';
+  tituloSubs$: Subscription | null = null;
+  
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `Admin - ${titulo}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$?.unsubscribe();
+  }
+
+  private getArgumentosRuta() {
+    return this.router.events
+      .pipe(
+        filter(event => event instanceof ActivationEnd),
+        filter((event: any) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data)
+      )
+  }
 
 }
